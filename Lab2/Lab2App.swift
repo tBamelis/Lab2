64b22c69de7323403517acf6455f02d0fecaecdb
Thibeau Bamelis <thibeau@iMacD.local>
2025-09-30 11:34:24 +0200

    first

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Thibeau Bamelis on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
